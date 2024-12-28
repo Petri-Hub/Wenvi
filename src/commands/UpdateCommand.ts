af00e89bf02f0c43341d145a048a3a9a1df86d64
Petri-Hub <fernando.petri01@gmail.com>
2024-12-28 11:34:38 -0300

    chore: rename 'update' command to 'upgrade'

--- src/commands/UpdateCommand.ts
+++ src/commands/UpdateCommand.ts
@@ -1,7 +1,7 @@
 import { ICommand } from "../interfaces/ICommand";
 import { execSync } from "child_process";
 
-export class UpdateCommand implements ICommand{
+export class UpgradeCommand implements ICommand{
     public async execute(): Promise<void> {
         execSync('npm install -g wenvi')
     }
