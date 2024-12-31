01963b224451f50e6ae0bd138262362aace776ac
Petri-Hub <fernando.petri01@gmail.com>
2024-12-31 15:08:18 -0300

    feat: make 'list' command prettier with chalk

--- src/commands/ListCommand.ts
+++ src/commands/ListCommand.ts
@@ -1,16 +1,34 @@
 import { ICommand } from "../interfaces/ICommand";
+import { Logger } from "../logging/Logger";
 import { CommandInput } from "../types/CommandInput";
+import chalk from 'chalk'
+import { TextToColorConverter } from '../helpers/TextToColorConverter'
+import { IRepository } from "../interfaces/IRepository";
 
 export class ListCommand implements ICommand {
     public async execute({ repository }: CommandInput): Promise<void> {
-        const environments = await repository.list()
-        const isEmpty = environments.length === 0
+        Logger.log('Configured environments:\n')
+        
+        const subjects = await repository.getSubjects()
 
-        if(isEmpty){
-            console.log('No environments found')
-            return
+        for(const subjectName of subjects){
+            const subjectColor = TextToColorConverter.convert(subjectName)
+            const subjectChalked = chalk.hex(subjectColor).bold(`[${subjectName}]`)
+
+            console.log(subjectChalked)
+
+            const environments = await repository.getEnvironments(subjectName)
+            const subjectIsEmpty = environments.length === 0
+
+            if(subjectIsEmpty){
+                console.log('  - No environments found')
+            }
+
+            for(const environmentName of environments){
+                console.log(`  - ${environmentName}`)
+            }
+            
+            console.log()
         }
-
-        console.log(environments)
     }
 }
