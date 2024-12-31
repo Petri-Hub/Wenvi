626bb671e402cbe08cdd9dc30c2a52817e9eb7ae
Petri-Hub <fernando.petri01@gmail.com>
2024-12-31 15:08:03 -0300

    feat: adjust repository methods and standardize method names

--- src/interfaces/IRepository.ts
+++ src/interfaces/IRepository.ts
@@ -1,11 +1,8 @@
-import { EnvironmentFile } from "../types/EnvironmentFile";
-
 export interface IRepository {
-   get(subject: string, environment: string): Promise<string | null>
-   list(): Promise<EnvironmentFile[]>
    exists(): Promise<boolean>
-   listSubjects(): Promise<string[]>
-   listEnvironments(subject: string): Promise<string[]>
+   getSubjects(): Promise<string[]>
+   getEnvironments(subject: string): Promise<string[]>
+   getEnvironment(subject: string, environment: string): Promise<string>
    getExample(): Promise<string>
    createExample(): Promise<void>
    createSubject(subject: string): Promise<void>
