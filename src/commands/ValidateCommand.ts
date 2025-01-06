f757ca44b01d22b563c1edf26ba25ebf795d873f
Petri <fernando.petri01@gmail.com>
2025-01-06 17:01:39 -0300

    feat: pretty 'validate' command to better see each enviroment validation

--- src/commands/ValidateCommand.ts
+++ src/commands/ValidateCommand.ts
@@ -1,45 +1,113 @@
+import Table from 'cli-table3'
+import { HorizontalTableRow } from "cli-table3";
 import { ICommand } from "../interfaces/ICommand";
 import { BaseCommand } from "./BaseCommand";
+import { EnvironmentValidation } from "../types/EnvironmentValidation";
+import { Logger } from "../logging/Logger";
+import chalk from 'chalk';
+import { TextToColorConverter } from '../helpers/TextToColorConverter';
 
 export class ValidateCommand extends BaseCommand implements ICommand{    
     public async execute(): Promise<void> {
+
+        const rows = await this.createRows()
+        const table = await this.createTable(rows)
+
+        Logger.log('Validations of subjects and environments:')
+        console.log(table.toString())
+    }
+
+    private async createRows(): Promise<HorizontalTableRow[]> {
+        const rows: HorizontalTableRow[] = []
+
         const repository = this.getRepository()
-        const example = await repository.getExample()
-
-        const variables = example
-            .split('\n')
-            .map(line => line.split('=').at(0))
-
         const subjects = await repository.getSubjects()
 
         for(const subjectName of subjects){
             const environments = await repository.getEnvironments(subjectName)
+            
+            for(const environmentName of environments){
+                const example = await this.getRequiredVariables()
+                const environment = await repository.getEnvironment(
+                    subjectName, 
+                    environmentName
+                )
 
-            console.group(subjectName)
+                const validation = await this.validateEnvironment(
+                    example, 
+                    environment
+                )
 
-            for(const environmentFileName of environments){
-                const missingVariables = []
+                const missingKeysColored = validation.missingKeys.map(key => {
+                    return chalk.red(key)
+                })
 
-                const environmentName = environmentFileName.split('.').pop() as string
-                const environment = await repository.getEnvironment(subjectName, environmentName) as string
-                const environmentVariables = environment
-                    .split('\n')
-                    .map(line => line.split('=').at(0))
+                const additionalKeys = validation.additionalKeys.map(key => {
+                    return chalk.yellow(key)
+                })
 
-                for(const variable of variables){
-                    if(!environmentVariables.includes(variable)){
-                        missingVariables.push(variable)
-                    }
-                }
-
-                if(missingVariables.length > 0){
-                    console.log(`- ${environmentName}: ${missingVariables.join(', ')}`)
-                } else {
-                    console.log(`- ${environmentName}: No missing variables`)
-                }
+                rows.push([
+                    { vAlign: 'center', content: environmentName },
+                    { vAlign: 'center', content: validation.isValid ? '✅' : '❌' },
+                    missingKeysColored.join('\n'),
+                    additionalKeys.join('\n')
+                ])
             }
 
-            console.groupEnd()
+            const subjectChalked = chalk.hex(TextToColorConverter.convert(subjectName)).bold(subjectName)
+            const firstSubjectRow =  rows.at(rows.length - environments.length)!
+
+            firstSubjectRow.unshift({ 
+                rowSpan: environments.length,
+                content: subjectChalked,
+                vAlign: 'center'
+            }) 
         }
+
+        return rows
+    }
+
+    private async validateEnvironment(example: string[], environment: string): Promise<EnvironmentValidation>{
+        const variables = environment
+            .split('\n')
+            .map(line => line.split('=').at(0)) as string[]
+
+        const missingKeys = example
+            .filter(exampleVariable => !variables.includes(exampleVariable))
+
+        const additionalKeys = variables
+            .filter(variable => !example.includes(variable))
+
+        const isValid = !missingKeys.length
+
+        return {
+            isValid,
+            missingKeys,
+            additionalKeys
+        }
+    }
+
+    private async getRequiredVariables(): Promise<string[]> {
+        const repository = this.getRepository()
+        const example = await repository.getExample()
+
+        return example
+            .split('\n')
+            .map(line => line.split('=').at(0)) as string[]
+    }
+
+    private async createTable(rows: HorizontalTableRow[]): Promise<Table.Table> {
+        const table = new Table({
+            head: ['Subject', 'Environment', 'Valid', 'Missing Keys', 'Additional Keys'],
+            wordWrap: true,
+            colAligns: ['left', 'left', 'center', 'left', 'left'],
+            style: {
+                head: ['white', 'white', 'white', 'red', 'yellow']
+            }        
+        })
+
+        table.push(...rows)
+
+        return table
     }
 }
