933946f5a43d44e773c497735f5f37971ea7b223
Petri-Hub <fernando.petri01@gmail.com>
2025-01-15 23:41:31 -0300

    feat: show example file in example command if file is already configured

--- src/commands/ExampleCommand.ts
+++ src/commands/ExampleCommand.ts
@@ -5,7 +5,28 @@ import { BaseCommand } from './BaseCommand'
 export class ExampleCommand extends BaseCommand implements IExecutableCommand {
    public async execute(): Promise<void> {
       const repository = this.getRepository()
+      
+      if(await repository.isExampleCreated()){
+         await this.showExampleFile()
+         return
+      }
+
+      await this.createExampleFile()
+   }
+
+   private async showExampleFile(): Promise<void> {
+      const repository = this.getRepository()
+      const example = await repository.getExample()
+
+      Logger.log("Seeing example file:\n")
+      console.log(example)
+   }
+
+   private async createExampleFile(): Promise<void> {
+      const repository = this.getRepository()
+
       await repository.createExample()
+
       Logger.success('Example file created sucessfully.')
    }
 }
